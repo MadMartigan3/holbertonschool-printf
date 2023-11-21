b99295ff867c2511dc6abf8ae5cc0586c7475c9c
malak33s <melaksarsarhani1bga@gmail.com>
2023-11-21 08:04:50 -0800

    test

--- functions.c
+++ functions.c
@@ -12,14 +12,40 @@ int print_char(va_list ap)
 
     i = va_arg(ap, int);
 
-    _putchar(i);
+    _putchar(i);2
 
     return(1);
 }
 
 /**
- * print_char - function that display a character
- * @ap: va list
- * Return: 1
+ * print_int - print an integer in base 10
+ * @ap: va  list
+ * Return: 1 success
  */
-int print_it(va_list ap)
+int print_int(va_list ap)
+	int i; 
+	i = va_arg(ap, int);
+{
+	_putchar(i);
+
+	return(1)
+}
+
+/**
+ * print_decimal - print a decimal (base 10) number
+ * @ap: va list
+ * Return: 1 success
+ */
+int print_decimal(va_list ap)
+	int i;
+	i = va_arg(ap, int)
+{
+	i  = 0;
+
+	if (i <= 0; <= 100; i++)
+	
+	else
+		(i >= 0;)
+			_putchar(i);
+}
+
